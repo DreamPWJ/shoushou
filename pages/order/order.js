c3dfbee21111541f992e83b66991a69965d1cc72
406798106@qq.com <406798106@qq.com>
2017-08-22 18:28:18 +0800

    收收信息供应者微信小程序

--- pages/order/order.js
+++ pages/order/order.js
@@ -8,14 +8,7 @@ Page({
      * 页面的初始数据
      */
     data: {
-        tabs: ['未完成订单', '所有订单'],
-        stv: {
-            windowWidth: 0,
-            lineWidth: 0,
-            offset: 0,
-            tStart: false
-        },
-        activeTab: 0,
+        currentTab: 0,   // tab切换
         page: 1,
         hasData: true,
         isNotData: true,
@@ -27,17 +20,6 @@ Page({
      */
     onLoad: function (options) {
         util.isLoginModal();
-        try {
-            let {tabs} = this.data;
-            var res = wx.getSystemInfoSync()
-            this.windowWidth = res.windowWidth;
-            this.data.stv.lineWidth = this.windowWidth / this.data.tabs.length;
-            this.data.stv.windowWidth = res.windowWidth;
-            this.setData({stv: this.data.stv})
-            this.tabsCount = tabs.length;
-        } catch (e) {
-            console.log(e);
-        }
         //获取消息列表
         this.getOrderList(this.data.page);
     },
@@ -108,18 +90,18 @@ Page({
             function (data) {
                 if (that.data.page == 1) {
                     that.setData({
-                        newsListArr: []
+                        orderListArr: []
                     })
                 }
 
                 for (var index in data.data.data_list) {
-                    that.data.newsListArr.push(data.data.data_list[index]);
+                    that.data.orderListArr.push(data.data.data_list[index]);
                 }
 
                 that.setData({
                     hasData: data.data.page_count == that.data.page ? false : true,
                     isNotData: (data.data == null || data.data.data_list == 0) ? true : false,
-                    newsList: that.data.newsListArr
+                    newsList: that.data.orderListArr
                 })
                 that.data.page++;
 
@@ -127,76 +109,17 @@ Page({
         )
     }
     ,
-    handlerStart(e) {
-        let {clientX, clientY} = e.touches[0];
-        this.startX = clientX;
-        this.tapStartX = clientX;
-        this.tapStartY = clientY;
-        this.data.stv.tStart = true;
-        this.tapStartTime = e.timeStamp;
-        this.setData({stv: this.data.stv})
-    },
-    handlerMove(e) {
-        let {clientX, clientY} = e.touches[0];
-        let {stv} = this.data;
-        let offsetX = this.startX - clientX;
-        this.startX = clientX;
-        stv.offset += offsetX;
-        if (stv.offset <= 0) {
-            stv.offset = 0;
-        } else if (stv.offset >= stv.windowWidth * (this.tabsCount - 1)) {
-            stv.offset = stv.windowWidth * (this.tabsCount - 1);
-        }
-        this.setData({stv: stv});
-    },
-    handlerCancel(e) {
+    /**
+     * 滑动切换tab
+     */
+    bindChange: function (e) {
+        this.setData({currentTab: e.detail.current});
 
     },
-    handlerEnd(e) {
-        let {clientX, clientY} = e.changedTouches[0];
-        let endTime = e.timeStamp;
-        let {tabs, stv, activeTab} = this.data;
-        let {offset, windowWidth} = stv;
-        //快速滑动
-        if (endTime - this.tapStartTime <= 300) {
-            //向左
-            if (Math.abs(this.tapStartY - clientY) < 50) {
-                if (this.tapStartX - clientX > 5) {
-                    if (activeTab < this.tabsCount - 1) {
-                        this.setData({activeTab: ++activeTab})
-                    }
-                } else {
-                    if (activeTab > 0) {
-                        this.setData({activeTab: --activeTab})
-                    }
-                }
-                stv.offset = stv.windowWidth * activeTab;
-            } else {
-                //快速滑动 但是Y距离大于50 所以用户是左右滚动
-                let page = Math.round(offset / windowWidth);
-                if (activeTab != page) {
-                    this.setData({activeTab: page})
-                }
-                stv.offset = stv.windowWidth * page;
-            }
-        } else {
-            let page = Math.round(offset / windowWidth);
-            if (activeTab != page) {
-                this.setData({activeTab: page})
-            }
-            stv.offset = stv.windowWidth * page;
-        }
-        stv.tStart = false;
-        this.setData({stv: this.data.stv})
-    },
-    _updateSelectedPage(page) {
-        let {tabs, stv, activeTab} = this.data;
-        activeTab = page;
-        this.setData({activeTab: activeTab})
-        stv.offset = stv.windowWidth * activeTab;
-        this.setData({stv: this.data.stv})
-    },
-    handlerTabTap(e) {
-        this._updateSelectedPage(e.currentTarget.dataset.index);
+    /**
+     * 点击tab切换
+     */
+    swichNav: function (e) {
+        util.swichNav(e, this)
     }
 })
