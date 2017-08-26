1827858f0bf4bb55cce8c0a8e5150f2b3cd2ad11
406798106@qq.com <406798106@qq.com>
2017-08-26 21:33:18 +0800

    收收信息供应者微信小程序

--- pages/index/infofee.js
+++ pages/index/infofee.js
@@ -118,7 +118,12 @@ Page({
                 that.setData({
                     hasData: data.data.page_count == that.data.page ? false : true,
                     isNotData: (data.data == null || data.data.data_list.length == 0) ? true : false,
-                    tradeList: that.data.tradeList
+                    tradeList: that.data.tradeList.map(function (item) {
+                        item.amount=util.formatMoney(item.amount,2);
+                        item.preamount=util.formatMoney(item.preamount,2);
+                        item.createtime= new Date(item.createtime).Format("yyyy-MM-dd hh:mm")
+                    return item
+                    })
                 })
                 that.data.page++;
             }
--- utils/util.js
+++ utils/util.js
@@ -137,6 +137,46 @@ function authorization(type, callback, immediately) {
 }
 
 /**
+ *   格式化时间
+ *  对Date的扩展，将 Date 转化为指定格式的String
+ *  月(M)、日(d)、小时(h)、分(m)、秒(s)、季度(q) 可以用 1-2 个占位符，
+ *  年(y)可以用 1-4 个占位符，毫秒(S)只能用 1 个占位符(是 1-3 位的数字)
+ *  例子：
+ * (new Date()).Format("yyyy-MM-dd hh:mm:ss.S") ==> 2006-07-02 08:09:04.423
+ *  (new Date()).Format("yyyy-M-d h:m:s.S")      ==> 2006-7-2 8:9:4.18
+ */
+Date.prototype.Format = function (fmt) { //
+    var o = {
+        "M+": this.getMonth() + 1, //月份
+        "d+": this.getDate(), //日
+        "h+": this.getHours(), //小时
+        "m+": this.getMinutes(), //分
+        "s+": this.getSeconds(), //秒
+        "q+": Math.floor((this.getMonth() + 3) / 3), //季度
+        "S": this.getMilliseconds() //毫秒
+    };
+    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    for (var k in o)
+        if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+    return fmt;
+}
+
+/**
+ * 格式化金额 s金额  n格式小数位数
+ */
+function formatMoney(s, n) {
+    n = n > 0 && n <= 20 ? n : 2;
+    s = parseFloat((s + "").replace(/[^\d\.-]/g, "")).toFixed(n) + "";
+    var l = s.split(".")[0].split("").reverse(),
+        r = s.split(".")[1];
+   var t = "";
+    for (var i = 0; i < l.length; i++) {
+        t += l[i] + ((i + 1) % 3 == 0 && (i + 1) != l.length ? "," : "");
+    }
+    return t.split("").reverse().join("") + "." + r;
+}
+
+/**
  * 是否登录
  */
 function isLogin() {
@@ -704,6 +744,7 @@ function makePhoneCall(that, callback) {
 module.exports = {
     https: https,
     authorization: authorization,
+    formatMoney: formatMoney,
     isLogin: isLogin,
     isLoginModal: isLoginModal,
     showToast: showToast,
