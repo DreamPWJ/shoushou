2e7e132c03d3136d06f4aaeb6e4f15e11dacd017
406798106@qq.com <406798106@qq.com>
2017-08-26 17:45:28 +0800

    收收信息供应者微信小程序

--- pages/index/infofee.js
+++ pages/index/infofee.js
@@ -6,12 +6,20 @@ Page({
     /**
      * 页面的初始数据
      */
-    data: {},
+    data: {
+        page: 1,
+        hasData: true,
+        tradeList: []
+    },
 
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
+        util.isLoginModal();
+        this.setData({
+            channel: options.channel||0
+        })
 
     },
 
@@ -26,8 +34,23 @@ Page({
      * 生命周期函数--监听页面显示
      */
     onShow: function () {
-        util.isLoginModal();
-        this.getInfoFee();
+        var that = this;
+        if (that.data.channel == 0) {
+            wx.setNavigationBarTitle({
+                title: "交易记录"
+            })
+        } else if (that.data.channel == 402) {
+            wx.setNavigationBarTitle({
+                title: "信息费收入"
+            })
+        }
+
+        that.setData({
+            page: 1
+        }, function () {
+            that.getInfoFee(1);
+        })
+
     },
 
     /**
@@ -48,14 +71,22 @@ Page({
      * 页面相关事件处理函数--监听用户下拉动作
      */
     onPullDownRefresh: function () {
-
+        var that = this;
+        that.setData({
+            page: 1
+        }, function () {
+            that.getInfoFee(1);
+        })
     },
 
     /**
      * 页面上拉触底事件的处理函数
      */
     onReachBottom: function () {
-
+        if (!this.data.hasData) {
+            return;
+        }
+        this.getInfoFee(this.data.page);
     },
 
     /**
@@ -67,15 +98,29 @@ Page({
     /**
      * 信息费数据 获取资金交易记录
      */
-    getInfoFee: function () {
+    getInfoFee: function (page) {
         var that = this;
-        util.https(app.globalData.api + "/api/subaccount/get_tradelist/"+wx.getStorageSync('userid'), "GET", {},
+        var data = {
+            page: page,//页码
+            size: 10,//条数
+            channel: that.data.channel == 0 ? "" : that.data.channel,
+        }
+        util.https(app.globalData.api + "/api/subaccount/get_tradelist/" + wx.getStorageSync('userid'), "GET", data,
             function (data) {
-                if (data.code == 1001) {
-
-                } else {
-                    util.toolTip(that, data.message);
+                if (that.data.page == 1) {
+                    that.setData({
+                        tradeList: []
+                    })
                 }
+                for (var index in data.data.data_list) {
+                    that.data.tradeList.push(data.data.data_list[index]);
+                }
+                that.setData({
+                    hasData: data.data.page_count == that.data.page ? false : true,
+                    isNotData: (data.data == null || data.data.data_list.length == 0) ? true : false,
+                    tradeList: that.data.tradeList
+                })
+                that.data.page++;
             }
         )
     }
