cefcfdd22e53078b17a7e7fa216f9f06d106f444
406798106@qq.com <406798106@qq.com>
2017-08-27 16:01:26 +0800

    收收信息供应者微信小程序

--- pages/wallet/withdraw.js
+++ pages/wallet/withdraw.js
@@ -72,4 +72,46 @@ Page({
     bindChange: function (e) {
         inputContent[e.currentTarget.id] = e.detail.value;
     },
+    /**
+     * 用户提现
+     */
+    withdrawSubmit: function (e) {
+
+        var that = this;
+        //验证表单
+        that.WxValidate = new WxValidate({
+                money: {  //验证规则 input name值
+                    required: true,
+                    money: true
+                }
+            },
+            {
+                money: { //提示信息
+                    required: "请填写提现金额",
+                },
+
+            })
+        util.wxValidate(e, that, function () {
+            //提现
+            util.https(app.globalData.api + "/api/subaccount/cash", "POST", {
+                    userbankid: "", //银行id
+                    userid: wx.getStorageSync("userid"),//用户userid
+                    amount: inputContent.money, //金额
+                },
+                function (data) {
+                    if (data.code == 1001) {
+                        util.toolTip(that, "工作日24小时之内到账", 1);
+                        wx.switchTab({
+                            url: '/pages/wallet/wallet'
+                        })
+                    } else {
+                        util.toolTip(that, data.message)
+                    }
+
+                }
+            )
+        })
+
+
+    }
 })
